facaf8ca4da58a34540f4e988fbd56b2ed6a129a
Colin Charleson <colin.charleson@ontariotechu.net>
2022-03-06 19:47:08 -0500

    baseGame

---
+++ src/Gameplay/Components/PlayerController.cpp
@@ -0,0 +1,83 @@
+#include "Gameplay/Components/PlayerController.h"
+#include <GLFW/glfw3.h>
+#define  GLM_SWIZZLE
+#include <GLM/gtc/quaternion.hpp>
+
+#include "Gameplay/GameObject.h"
+#include "Gameplay/Scene.h"
+#include "Utils/JsonGlmHelpers.h"
+#include "Utils/ImGuiHelper.h"
+#include "Gameplay/InputEngine.h"
+#include "Application/Application.h"
+
+PlayerController::PlayerController() :
+	IComponent(),
+	_mouseSensitivity({ 0.5f, 0.3f }),
+	_moveSpeeds(glm::vec3(1.0f)),
+	_shiftMultipler(2.0f),
+	_currentRot(glm::vec2(0.0f)),
+	_isMousePressed(false)
+{ }
+
+PlayerController::~PlayerController() = default;
+
+void PlayerController::Update(float deltaTime)
+{
+	if (Application::Get().IsFocused) {
+		if (InputEngine::GetMouseState(GLFW_MOUSE_BUTTON_LEFT) == ButtonState::Pressed) {
+			_prevMousePos = InputEngine::GetMousePos();
+			LOG_INFO("doot");
+		}
+
+		
+			
+			glm::vec3 input = glm::vec3(0.0f);
+			if (InputEngine::IsKeyDown(GLFW_KEY_W)) {
+				playerY += 2 * deltaTime;
+				GetGameObject()->SetRotation(glm::vec3(0, 0, -90));
+			}
+			if (InputEngine::IsKeyDown(GLFW_KEY_S)) {
+				playerY -= 2 * deltaTime;
+				GetGameObject()->SetRotation(glm::vec3(0, 0, 90));
+			}
+			if (InputEngine::IsKeyDown(GLFW_KEY_A)) {
+				playerX -= 2 * deltaTime;
+				GetGameObject()->SetRotation(glm::vec3(0, 0, 0));
+			}
+			if (InputEngine::IsKeyDown(GLFW_KEY_D)) {
+				playerX += 2 * deltaTime;
+				GetGameObject()->SetRotation(glm::vec3(0, 0, 180));
+			}
+
+			if (InputEngine::IsKeyDown(GLFW_KEY_LEFT_SHIFT)) {
+				input *= _shiftMultipler;
+			}
+
+			GetGameObject()->SetPostion(glm::vec3(playerX, playerY, 1));
+		
+	}
+	_prevMousePos = InputEngine::GetMousePos();
+}
+
+void PlayerController::RenderImGui()
+{
+	LABEL_LEFT(ImGui::DragFloat2, "Mouse Sensitivity", &_mouseSensitivity.x, 0.01f);
+	LABEL_LEFT(ImGui::DragFloat3, "Move Speed       ", &_moveSpeeds.x, 0.01f, 0.01f);
+	LABEL_LEFT(ImGui::DragFloat , "Shift Multiplier ", &_shiftMultipler, 0.01f, 1.0f);
+}
+
+nlohmann::json PlayerController::ToJson() const {
+	return {
+		{ "mouse_sensitivity", _mouseSensitivity },
+		{ "move_speed", _moveSpeeds },
+		{ "shift_mult", _shiftMultipler }
+	};
+}
+
+PlayerController::Sptr PlayerController::FromJson(const nlohmann::json& blob) {
+	PlayerController::Sptr result = std::make_shared<PlayerController>();
+	result->_mouseSensitivity = JsonGet(blob, "mouse_sensitivity", result->_mouseSensitivity);
+	result->_moveSpeeds       = JsonGet(blob, "move_speed", result->_moveSpeeds);
+	result->_shiftMultipler   = JsonGet(blob, "shift_mult", 2.0f);
+	return result;
+}
---
+++ src/Gameplay/Components/PlayerController.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "IComponent.h"
+
+struct GLFWwindow;
+
+/// <summary>
+/// A simple behaviour that allows movement of a gameobject with WASD, mouse,
+/// and ctrl + space
+/// </summary>
+class PlayerController : public Gameplay::IComponent {
+public:
+	typedef std::shared_ptr<PlayerController> Sptr;
+
+	PlayerController();
+	virtual ~PlayerController();
+
+	virtual void Update(float deltaTime) override;
+
+public:
+	virtual void RenderImGui() override;
+	MAKE_TYPENAME(PlayerController);
+	virtual nlohmann::json ToJson() const override;
+	static PlayerController::Sptr FromJson(const nlohmann::json& blob);
+
+protected:
+	float _shiftMultipler;
+	glm::vec2 _mouseSensitivity;
+	glm::vec3 _moveSpeeds;
+	glm::dvec2 _prevMousePos;
+	glm::vec2 _currentRot;
+
+	float playerX = 0, playerY = 0;
+	bool _isMousePressed = false;
+};
